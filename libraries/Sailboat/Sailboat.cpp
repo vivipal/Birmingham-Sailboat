e05efa7ffce0f7d2d1e480c67d95a0ae4dcd1a3a
VP <virgile.pelle@ensta-bretagne.org>
2022-06-13 10:32:27 +0100

    add cpp file for sailboat object

---
+++ libraries/Sailboat/Sailboat.cpp
@@ -0,0 +1,52 @@
+#include "Sailboat.h"
+
+
+
+Sailboat::Sailboat() {
+  m_sensor[NAME_IMU] =  new CMPS12();
+  m_sensor[NAME_GPS] = new GPS();
+  m_sensor[NAME_RC] = new RC();
+  m_sensor[NAME_WDIRECTION] = new WindDirection();
+  m_sensor[NAME_WSPEED] = new WindSpeed();
+
+  m_servo[NAME_RUDDER] = new Servo_Motor(RUDDER_PIN, RUDDER_PWMMIN, RUDDER_PWMMAX, RUDDER_ANGLEMIN, RUDDER_ANGLEMAX);
+  m_servo[NAME_SAIL] = new Servo_Motor(SAIL_PIN, SAIL_PWMMIN, SAIL_PWMMAX, SAIL_ANGLEMIN, SAIL_ANGLEMAX);
+};
+
+Sailboat::~Sailboat() {
+  for (size_t i = 0; i < NB_SENSORS; i++) {
+    delete m_sensor[i];
+  }
+}
+
+void Sailboat::init(Adafruit_PWMServoDriver* servos_pwm){
+  for (size_t i = 0; i < NB_SENSORS; i++) {
+    m_sensor[i]->init();
+  }
+
+  for (size_t i = 0; i < NB_SERVOS; i++) {
+    m_servo[i]->init(servos_pwm);
+  }
+}
+
+WindDirection* Sailboat::wd(){return (WindDirection*) m_sensor[NAME_WDIRECTION];};
+WindSpeed* Sailboat::ws(){return (WindSpeed*) m_sensor[NAME_WSPEED];};
+CMPS12* Sailboat::compass(){return (CMPS12*) m_sensor[NAME_IMU];};
+GPS* Sailboat::gps(){return (GPS*) m_sensor[NAME_GPS];};
+RC* Sailboat::rc(){return (RC*) m_sensor[NAME_RC];};
+
+Servo_Motor* Sailboat::rudderServo(){return (Servo_Motor*) m_servo[NAME_RUDDER];};
+Servo_Motor* Sailboat::sailServo(){return (Servo_Motor*) m_servo[NAME_SAIL];};
+
+void Sailboat::updateSensors(){
+  for (size_t i = 0; i < NB_SENSORS; i++) {
+    m_sensor[i]->update();
+  }
+}
+
+void Sailboat::updateServos(){
+    m_servo[NAME_SAIL]->set( rc()->getValue(SAIL_CHANNEL) );
+    m_servo[NAME_RUDDER]->set( rc()->getValue(RUDDER_CHANNEL) );
+}
+
+int Sailboat::controlMode(){return rc()->isReceiving() ? RADIO_CONTROLLED : AUTONOMOUS;}
--- libraries/Sailboat/Sailboat.h
+++ libraries/Sailboat/Sailboat.h
@@ -18,54 +18,25 @@
 class Sailboat {
 
   public:
-    Sailboat() {
-      m_sensor[NAME_IMU] =  new CMPS12();
-      m_sensor[NAME_GPS] = new GPS();
-      m_sensor[NAME_RC] = new RC();
-      m_sensor[NAME_WDIRECTION] = new WindDirection();
-      m_sensor[NAME_WSPEED] = new WindSpeed();
+    Sailboat();
 
-      m_servo[NAME_RUDDER] = new Servo_Motor(RUDDER_PIN, RUDDER_PWMMIN, RUDDER_PWMMAX, RUDDER_ANGLEMIN, RUDDER_ANGLEMAX);
-      m_servo[NAME_SAIL] = new Servo_Motor(SAIL_PIN, SAIL_PWMMIN, SAIL_PWMMAX, SAIL_ANGLEMIN, SAIL_ANGLEMAX);
-    };
+    ~Sailboat();
 
-    ~Sailboat() {
-      for (size_t i = 0; i < NB_SENSORS; i++) {
-        delete m_sensor[i];
-      }
-    }
+    void init(Adafruit_PWMServoDriver* servos_pwm);
 
-    void init(Adafruit_PWMServoDriver* servos_pwm){
-      for (size_t i = 0; i < NB_SENSORS; i++) {
-        m_sensor[i]->init();
-      }
+    WindDirection* wd();
+    WindSpeed* ws();
+    CMPS12* compass();
+    GPS* gps();
+    RC* rc();
 
-      for (size_t i = 0; i < NB_SERVOS; i++) {
-        m_servo[i]->init(servos_pwm);
-      }
-    }
+    Servo_Motor* rudderServo();
+    Servo_Motor* sailServo();
 
-    WindDirection* wd(){return (WindDirection*) m_sensor[NAME_WDIRECTION];};
-    WindSpeed* ws(){return (WindSpeed*) m_sensor[NAME_WSPEED];};
-    CMPS12* compass(){return (CMPS12*) m_sensor[NAME_IMU];};
-    GPS* gps(){return (GPS*) m_sensor[NAME_GPS];};
-    RC* rc(){return (RC*) m_sensor[NAME_RC];};
+    void updateSensors();
 
-    Servo_Motor* rudderServo(){return (Servo_Motor*) m_servo[NAME_RUDDER];};
-    Servo_Motor* sailServo(){return (Servo_Motor*) m_servo[NAME_SAIL];};
-
-    void updateSensors(){
-      for (size_t i = 0; i < NB_SENSORS; i++) {
-        m_sensor[i]->update();
-      }
-    }
-
-    void updateServos(){
-        m_servo[NAME_SAIL]->set( rc()->getValue(SAIL_CHANNEL) );
-        m_servo[NAME_RUDDER]->set( rc()->getValue(RUDDER_CHANNEL) );
-    }
-
-    int controlMode(){return rc()->isReceiving() ? RADIO_CONTROLLED : AUTONOMOUS;}
+    void updateServos();
+    int controlMode();
 
 
 

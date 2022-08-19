3f9179c4fba77c18a7f89f6258504007a8625e2b
VP <virgile.pelle@ensta-bretagne.org>
2022-08-19 14:54:34 +0100

    disable servos test on setup

--- Sailboat/Sailboat.ino
+++ Sailboat/Sailboat.ino
@@ -42,7 +42,7 @@ void setup(){
   Serial.println("Setup done"); Serial.println("----------\n");
   XBEE_SERIAL.println("Setup done\n\r----------\n");
 
-  boat.testServos();
+  //boat.testServos();
 }
 
 
